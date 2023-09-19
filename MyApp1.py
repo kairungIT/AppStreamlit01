886195467a07b4970f4167cb0fd89a25ff592996
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-09-19 12:12:27 +0700

    .commit

--- MyApp1.py
+++ MyApp1.py
@@ -9,6 +9,6 @@ col1, col2 = st.columns(2)
 #col1.write("This is column 1")
 #col2.write("This is column 2")
 with col1:
-    st.image('./pic/rung.jpg')
+    st.image('./pic/rung1.jfif')
 with col2:
-    st.image('./pic/iris.jpg')
+    st.image('./pic/iris2.jpg')
