b89460318e53f1296eb3b17f3f7b4bf949e220e8
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-09-19 12:02:16 +0700

    .commit

--- MyApp1.py
+++ MyApp1.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
-st.title('kairung')
-st.header('kairung')
-st.subheader('kairung')
+st.title('การทดสอบเขียนเว็บด้วย Python')
+st.header('ผศ.ดร.ไก้รุ่ง เฮงพระพรหม')
+st.subheader('สาขาวิชาวิทยาการข้อมูล')
+st.markdown("----")
