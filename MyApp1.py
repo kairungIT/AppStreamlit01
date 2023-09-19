a7e2f7371bac4fd03cb5df0bda2978c52b7dc81d
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-09-19 12:55:44 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -75,11 +75,14 @@ if st.button("ทำนายผล"):
    out=Knn_model.predict(x_input)
 
    if out[0]=="Setosa":
-      st.image("./pic/iris.jpg")
+      #st.image("./pic/iris.jpg")
+      st.header("Setosa")
    elif out[0]=="Versicolor":
-      st.image("./pic/iris2.jpg")
+      #st.image("./pic/iris2.jpg")
+      st.header("Versicolor")
    else:
-      st.image("./pic/iris1.jpg")  
+      #st.image("./pic/iris1.jpg")  
+      st.header("Verginiga")
    st.button("ไม่ทำนายผล")
 else :
     st.button("ไม่ทำนายผล")
